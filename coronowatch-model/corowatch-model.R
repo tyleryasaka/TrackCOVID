a0de0e6647b586b1528f67c75359ebeb29a44771
Tyler Yasaka <contact@tyleryasaka.me>
2020-03-19 15:49:17 -0700

    Update corowatch-model.R

--- coronowatch-model/corowatch-model.R
+++ coronowatch-model/corowatch-model.R
@@ -7,19 +7,11 @@ activeTime = 2
 infectionProb = 0.25
 probAwayMin = 0.7
 probAwayMax = 0.9
-
-placePopularities = rbeta(nPlaces, 2, 2)
+nTrials = 100
 
 booleanProb = function(probTrue, n=1) {
   sample(c(T,F), n, replace=T, c(probTrue, 1 - probTrue))
 }
-
-peopleHomes = floor(runif(nPeople, min=0, max=nPlaces))
-extroversions = runif(nPeople, min=probAwayMin, max=probAwayMax)
-peopleLocations = floor(runif(nPeople, min=0, max=nPlaces))
-peopleAtHome = rep(NA, nPeople)
-infected = booleanProb(initialInfected, n=nPeople)
-infectedTime = ifelse(infected, 0, NA)
 
 updatePersonAtHome = function(personIndex) {
   probAway = extroversions[personIndex]
@@ -59,35 +51,47 @@ isExposed = function(personIndex, exposedPlaces) {
   )
 }
 
-placeProbabilities = matrix(nrow=nPeople, ncol=nPlaces)
-for (personIndex in 1:nPeople) {
-  placeProbabilities[personIndex,] = getPlaceProbabilities(personIndex)
-}
+trialResults = c()
+for (q in 1:nTrials) {
+  infectedStart = length(infected[infected])
+  placePopularities = rbeta(nPlaces, 2, 2)
+  peopleHomes = floor(runif(nPeople, min=0, max=nPlaces))
+  extroversions = runif(nPeople, min=probAwayMin, max=probAwayMax)
+  peopleLocations = floor(runif(nPeople, min=0, max=nPlaces))
+  peopleAtHome = rep(NA, nPeople)
+  infected = booleanProb(initialInfected, n=nPeople)
+  infectedTime = ifelse(infected, 0, NA)
 
-infectedStart = length(infected[infected])
-for (t in 1:totalTime) {
-  exposedPlaces = c()
+  placeProbabilities = matrix(nrow=nPeople, ncol=nPlaces)
   for (personIndex in 1:nPeople) {
-    # update locations (simulate movement)
-    peopleAtHome[personIndex] = updatePersonAtHome(personIndex)
-    peopleLocations[personIndex] = updatePersonLocation(personIndex)
-    if (isActiveInfected(personIndex)) {
-      # if person is infected and active, they have exposed everyone at this location at the same time
-      exposedPlaces = append(exposedPlaces, peopleLocations[personIndex])
-    }
+    placeProbabilities[personIndex,] = getPlaceProbabilities(personIndex)
   }
-  for (personIndex in 1:nPeople) {
-    if (isExposed(personIndex, exposedPlaces)) {
-      # if person is in same time/place, they are exposed
-      # probalistic infection
-      infected[personIndex] = sample(booleanProb(infectionProb))
-      if (infected[personIndex]) {
-        # if infected, set time of infection
-        infectedTime[personIndex] = t
+
+  for (t in 1:totalTime) {
+    exposedPlaces = c()
+    for (personIndex in 1:nPeople) {
+      # update locations (simulate movement)
+      peopleAtHome[personIndex] = updatePersonAtHome(personIndex)
+      peopleLocations[personIndex] = updatePersonLocation(personIndex)
+      if (isActiveInfected(personIndex)) {
+        # if person is infected and active, they have exposed everyone at this location at the same time
+        exposedPlaces = append(exposedPlaces, peopleLocations[personIndex])
       }
     }
+    for (personIndex in 1:nPeople) {
+      if (isExposed(personIndex, exposedPlaces)) {
+        # if person is in same time/place, they are exposed
+        # probalistic infection
+        infected[personIndex] = sample(booleanProb(infectionProb))
+        if (infected[personIndex]) {
+          # if infected, set time of infection
+          infectedTime[personIndex] = t
+        }
+      }
+    }
+    # print(infected)
   }
-  # print(infected)
+  trialResults = append(trialResults, length(infected[infected]))
 }
-print(infectedStart)
-print(length(infected[infected]))
+# print(infectedStart)
+print(paste(mean(trialResults), round(sd(trialResults) * 100) / 100, sep=' +- '))
