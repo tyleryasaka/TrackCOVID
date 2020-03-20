dd7df3ae2854572d687d42f5b68bd3d969269ead
Tyler Yasaka <contact@tyleryasaka.me>
2020-03-20 13:33:23 -0700

    Refactor

--- covidwatch-model/covidwatch-model.R
+++ covidwatch-model/covidwatch-model.R
@@ -23,104 +23,10 @@ initialConfig = list(
   interventionCompliance = 0.75
 )
 
-setConfig = function(input) {
-  return(list(
-    nTrials = input$nTrials,
-    toggleIntervention = input$toggleIntervention,
-    genPlot = initialConfig$genPlot,
-    
-    # model config
-    nPlaces = initialConfig$nPlaces,
-    nPeople = initialConfig$nPeople,
-    totalTime = initialConfig$totalTime,
-    initialInfected = input$initialInfected,
-    activeTime = input$activeTime,
-    infectionProb = input$infectionProb,
-    probDiscoverInfection = input$probDiscoverInfection,
-    isolationCompliance = input$isolationCompliance,
-    
-    # intervention config
-    assumedTimeFromInfect = input$assumedTimeFromInfect | initialConfig$assumedTimeFromInfect,
-    putativeInfectProb = input$putativeInfectProb | initialConfig$putativeInfectProb,
-    interventionCompliance = input$interventionCompliance | initialConfig$interventionCompliance
-  ))
-}
-
 # ------------------------------------------------------- #
 #### Libraries ####
 # ------------------------------------------------------- #
-
 library(igraph)
-library(shiny)
-
-# ------------------------------------------------------- #
-#### UI ####
-# ------------------------------------------------------- #
-ui <- fluidPage(
-  mainPanel(
-    fluidRow(
-      align = "center",
-      titlePanel("Infection Spread With and Without Intervention")
-    ),
-    fluidRow(
-      align = "center",
-      checkboxInput("toggleIntervention", "Use Intervention", value = initialConfig$toggleIntervention),
-      selectInput("nTrials", "# of Simulations", 1:initialConfig$maxNTrials, selected = initialConfig$nTrials)
-    ),
-    fluidRow(
-      align = "center",
-      h3('Model Parameters')
-    ),
-    fluidRow(
-      column(
-        3,
-        sliderInput("initialInfected", h3("Initial Infected Proportion"), min = 0, max = 1, value = initialConfig$initialInfected),
-        sliderInput("isolationCompliance", h3("Isolation Compliance"), min = 0, max = 1, value = initialConfig$isolationCompliance)
-      ),
-      column(
-        3,
-        sliderInput("activeTime", h3("Active Time"), min = 0, max = 20, value = initialConfig$activeTime)
-      ),
-      column(
-        3,
-        sliderInput("infectionProb", h3("Transmission Rate"), min = 0, max = 1, value = initialConfig$infectionProb)
-      ),
-      column(
-        3,
-        sliderInput("probDiscoverInfection", h3("Discovery Rate"), min = 0, max = 1, value = initialConfig$probDiscoverInfection)
-      )
-    ),
-    fluidRow(
-      align = "center",
-      h3('Intervention Parameters')
-    ),
-    fluidRow(
-      column(
-        3,
-        sliderInput("assumedTimeFromInfect", h3("Estimated Time from Infection"), min = 0, max = 20, value = initialConfig$assumedTimeFromInfect)
-      ),
-      column(
-        3,
-        sliderInput("putativeInfectProb", h3("Estimated Transmission Rate"), min = 0, max = 1, value = initialConfig$putativeInfectProb)
-      ),
-      column(
-        3,
-        sliderInput("interventionCompliance", h3("Intervention Compliance"), min = 0, max = 1, value = initialConfig$interventionCompliance)
-      )
-    ),
-    fluidRow(
-      align = "center",
-      verbatimTextOutput("percentInfectedMean"),
-      uiOutput('selectSim'),
-      verbatimTextOutput("percentInfectedCurrent")
-    ),
-    fluidRow(
-      align = "center",
-      plotOutput('plot1')
-    ),
-    width = 12
-  )
-)
 
 # ------------------------------------------------------- #
 #### Methods ####
@@ -176,15 +82,15 @@ getPlaceProbabilities = function(personIndex, context, config) {
 isActiveInfected = function(personIndex, context, config, t) {
   return(
     context$infected[personIndex] &
-    (t - context$infectedTime[personIndex] <= config$activeTime)
+      (t - context$infectedTime[personIndex] <= config$activeTime)
   )
 }
 
 isExposed = function(personIndex, context) {
   return(
     !context$infected[personIndex] &
-    !context$peopleAtHome[personIndex] &
-    context$peopleLocations[personIndex] %in% context$exposedPlaces
+      !context$peopleAtHome[personIndex] &
+      context$peopleLocations[personIndex] %in% context$exposedPlaces
   )
 }
 
@@ -235,6 +141,29 @@ flagInfection = function(personIndex, t, context, config) {
     context$flaggedExposeEvents = append(context$flaggedExposeEvents, getVertexIndex(flaggedLocation, u, config))
   }
   return(context$flaggedExposeEvents[!is.na(context$flaggedExposeEvents)])
+}
+
+setConfig = function(input) {
+  return(list(
+    nTrials = input$nTrials,
+    toggleIntervention = input$toggleIntervention,
+    genPlot = initialConfig$genPlot,
+    
+    # model config
+    nPlaces = initialConfig$nPlaces,
+    nPeople = initialConfig$nPeople,
+    totalTime = initialConfig$totalTime,
+    initialInfected = input$initialInfected,
+    activeTime = input$activeTime,
+    infectionProb = input$infectionProb,
+    probDiscoverInfection = input$probDiscoverInfection,
+    isolationCompliance = input$isolationCompliance,
+    
+    # intervention config
+    assumedTimeFromInfect = input$assumedTimeFromInfect | initialConfig$assumedTimeFromInfect,
+    putativeInfectProb = input$putativeInfectProb | initialConfig$putativeInfectProb,
+    interventionCompliance = input$interventionCompliance | initialConfig$interventionCompliance
+  ))
 }
 
 # ------------------------------------------------------- #
@@ -341,59 +270,3 @@ modelFn = function(input) {
   }
   return(simulationResults)
 }
-
-server <- function(input, output) {
-  values <- reactiveValues(currentSim = 1)
-  observeEvent(input$currentSim, {
-    values$currentSim <- as.numeric(input$currentSim)
-  })
-  simulationResults = reactive(modelFn(input))
-  currentResult = reactive(simulationResults()[[values$currentSim]])
-  output$plot1 <- renderPlot({
-    return(list(
-      plot=plot(
-        currentResult()$exposureNetwork,
-        layout=function(g) { return(layout_on_grid(g, width=currentResult()$nPlaces)) },
-        vertex.color=ifelse(
-          currentResult()$exposeEvents,
-          '#e37d7d',
-          '#86bdfc'
-        ),
-        vertex.size=rep(sqrt(currentResult()$placePopularities * 100), currentResult()$totalTime),
-        edge.width=0.5,
-        edge.color=ifelse(
-          currentResult()$infectedMovements,
-          '#cf1111',
-          '#076adb'
-        ),
-        label.font=2
-      ),
-      infected=currentResult()$infected,
-      nTrials=currentResult()$nTrials
-    ))
-  }, height = 800, width = 800)
-  output$percentInfectedMean <- renderPrint(
-    {paste(
-      'Average of ',
-      currentResult()$nTrials,
-      ' Simulations: ',
-      paste(round(mean(sapply(simulationResults(), function(l) l$infected)) * 100), '%', sep=''),
-      ' Infected',
-      sep=''
-    )}
-  )
-  output$percentInfectedCurrent <- renderPrint(
-    {paste(
-      'Selected Simulation: ',
-      round(currentResult()$infected * 100),
-      '% Infected',
-      sep=''
-    )}
-  )
-  output$selectSim = renderUI({
-    selectInput("currentSim", "Show Simulation #", 1:currentResult()$nTrials, selected = values$currentSim)
-  })
-  
-}
-
-shinyApp(ui, server)
---
+++ covidwatch-model/covidwatch-ui.R
@@ -0,0 +1,134 @@
+# ------------------------------------------------------- #
+#### Libraries ####
+# ------------------------------------------------------- #
+library(shiny)
+library(igraph)
+source('./covidwatch-model.R')
+
+# ------------------------------------------------------- #
+#### UI ####
+# ------------------------------------------------------- #
+ui <- fluidPage(
+  mainPanel(
+    fluidRow(
+      align = "center",
+      titlePanel("Infection Spread With and Without Intervention")
+    ),
+    fluidRow(
+      align = "center",
+      checkboxInput("toggleIntervention", "Use Intervention", value = initialConfig$toggleIntervention),
+      selectInput("nTrials", "# of Simulations", 1:initialConfig$maxNTrials, selected = initialConfig$nTrials)
+    ),
+    fluidRow(
+      align = "center",
+      h3('Model Parameters')
+    ),
+    fluidRow(
+      column(
+        3,
+        sliderInput("initialInfected", h3("Initial Infected Proportion"), min = 0, max = 1, value = initialConfig$initialInfected),
+        sliderInput("isolationCompliance", h3("Isolation Compliance"), min = 0, max = 1, value = initialConfig$isolationCompliance)
+      ),
+      column(
+        3,
+        sliderInput("activeTime", h3("Active Time"), min = 0, max = 20, value = initialConfig$activeTime)
+      ),
+      column(
+        3,
+        sliderInput("infectionProb", h3("Transmission Rate"), min = 0, max = 1, value = initialConfig$infectionProb)
+      ),
+      column(
+        3,
+        sliderInput("probDiscoverInfection", h3("Discovery Rate"), min = 0, max = 1, value = initialConfig$probDiscoverInfection)
+      )
+    ),
+    fluidRow(
+      align = "center",
+      h3('Intervention Parameters')
+    ),
+    fluidRow(
+      column(
+        3,
+        sliderInput("assumedTimeFromInfect", h3("Estimated Time from Infection"), min = 0, max = 20, value = initialConfig$assumedTimeFromInfect)
+      ),
+      column(
+        3,
+        sliderInput("putativeInfectProb", h3("Estimated Transmission Rate"), min = 0, max = 1, value = initialConfig$putativeInfectProb)
+      ),
+      column(
+        3,
+        sliderInput("interventionCompliance", h3("Intervention Compliance"), min = 0, max = 1, value = initialConfig$interventionCompliance)
+      )
+    ),
+    fluidRow(
+      align = "center",
+      verbatimTextOutput("percentInfectedMean"),
+      uiOutput('selectSim'),
+      verbatimTextOutput("percentInfectedCurrent")
+    ),
+    fluidRow(
+      align = "center",
+      plotOutput('plot1')
+    ),
+    width = 12
+  )
+)
+
+# ------------------------------------------------------- #
+#### Server ####
+# ------------------------------------------------------- #
+server <- function(input, output) {
+  values <- reactiveValues(currentSim = 1)
+  observeEvent(input$currentSim, {
+    values$currentSim <- as.numeric(input$currentSim)
+  })
+  simulationResults = reactive(modelFn(input))
+  currentResult = reactive(simulationResults()[[values$currentSim]])
+  output$plot1 <- renderPlot({
+    return(list(
+      plot=plot(
+        currentResult()$exposureNetwork,
+        layout=function(g) { return(layout_on_grid(g, width=currentResult()$nPlaces)) },
+        vertex.color=ifelse(
+          currentResult()$exposeEvents,
+          '#e37d7d',
+          '#86bdfc'
+        ),
+        vertex.size=rep(sqrt(currentResult()$placePopularities * 100), currentResult()$totalTime),
+        edge.width=0.5,
+        edge.color=ifelse(
+          currentResult()$infectedMovements,
+          '#cf1111',
+          '#076adb'
+        ),
+        label.font=2
+      ),
+      infected=currentResult()$infected,
+      nTrials=currentResult()$nTrials
+    ))
+  }, height = 800, width = 800)
+  output$percentInfectedMean <- renderPrint(
+    {paste(
+      'Average of ',
+      currentResult()$nTrials,
+      ' Simulations: ',
+      paste(round(mean(sapply(simulationResults(), function(l) l$infected)) * 100), '%', sep=''),
+      ' Infected',
+      sep=''
+    )}
+  )
+  output$percentInfectedCurrent <- renderPrint(
+    {paste(
+      'Selected Simulation: ',
+      round(currentResult()$infected * 100),
+      '% Infected',
+      sep=''
+    )}
+  )
+  output$selectSim = renderUI({
+    selectInput("currentSim", "Show Simulation #", 1:currentResult()$nTrials, selected = values$currentSim)
+  })
+  
+}
+
+shinyApp(ui, server)
