e01e0da9cf704b8c70886e61c806d5f70d9f3a27
Tyler Yasaka <contact@tyleryasaka.me>
2020-03-27 19:15:15 -0700

    Create authenticated admin dashboard
    The dashboard can generate QR confirmation codes

--- covidwatch-server/index.js
+++ covidwatch-server/index.js
@@ -3,12 +3,22 @@ const express = require('express')
 const bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 const sha256 = require('js-sha256').sha256
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+const flash = require('connect-flash')
+const cookieParser = require('cookie-parser')
+const session = require('cookie-session')
 
 const maxDepth = 100
 
 const app = express()
 const port = process.env.PORT || 3000
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(session({ keys: [process.env.SESSION_KEY] }))
+app.use(flash())
+app.use('/admin/', express.static('admin-public'))
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/checkpoints', { useNewUrlParser: true })
 const db = mongoose.connection
@@ -18,8 +28,31 @@ const checkpointSchema = new mongoose.Schema({
   links: [String],
   exposed: Boolean
 })
+const userSchema = new mongoose.Schema({})
+const confirmcodeSchema = new mongoose.Schema({
+  code: String,
+  redeemed: Boolean
+})
+userSchema.plugin(passportLocalMongoose)
 
 const Checkpoint = mongoose.model('Checkpoint', checkpointSchema)
+const User = mongoose.model('User', userSchema)
+const Confirmcode = mongoose.model('Confirmcode', confirmcodeSchema)
+
+// Uncomment and run this file once to generate an admin user
+// const newUsername = 'user'
+// const newPass = 'pass'
+// const newUser = new User({ username: newUsername })
+// User.register(newUser, newPass, function () {
+//   console.log(`Registered user ${newUsername}`)
+// })
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 async function findExposures (checkpointKey, depth) {
   return new Promise(function (resolve) {
@@ -48,6 +81,34 @@ async function findExposures (checkpointKey, depth) {
     })
   })
 }
+
+app.post('/api/admin/login', passport.authenticate('local', { failureRedirect: '/admin/login.html', failureFlash: true }), function (req, res) {
+  res.redirect('/admin/dashboard.html')
+})
+
+app.get('/api/admin/logout', function (req, res) {
+  req.logout()
+  res.redirect('/admin/login.html')
+})
+
+app.get('/api/admin/status', function (req, res) {
+  res.send({ isLoggedIn: Boolean(req.user) })
+})
+
+app.post('/api/admin/confirmcode/generate', passport.authenticate('local', { failureRedirect: '/admin/login.html' }), (req, res) => {
+  const confirmcode = new Confirmcode({
+    code: sha256(String(Math.random())).substring(0, 32),
+    redeemed: false
+  })
+  confirmcode.save(function (err, confirmcode) {
+    if (err) {
+      console.error(err)
+      res.send({ error: true })
+    } else {
+      res.redirect(`/admin/confirmation-code.html?code=${confirmcode.code}`)
+    }
+  })
+})
 
 app.post('/checkpoints', (req, res) => {
   const checkpoint = new Checkpoint({
