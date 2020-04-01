446170623ad53daa318d5d739dc51af9411fd075
Tyler Yasaka <contact@tyleryasaka.me>
2020-03-31 20:54:24 -0700

    Web UI - set whether to use confirmed reports

--- covidwatch-web/src/Settings.js
+++ covidwatch-web/src/Settings.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import CheckBoxOutlineBlankOutlinedIcon from '@material-ui/icons/CheckBoxOutlineBlankOutlined'
+import CheckBoxIcon from '@material-ui/icons/CheckBox'
 import StatusAlert from './StatusAlert'
+import API from './api'
 
-const initialState = {}
+const initialState = {
+  useConfirmed: false
+}
 
 class Settings extends React.Component {
   constructor () {
@@ -11,10 +20,20 @@ class Settings extends React.Component {
   }
 
   componentDidMount () {
+    API.getUseConfirmed().then(useConfirmed => {
+      this.setState({ useConfirmed })
+    })
+  }
+
+  async toggleUseConfirmed () {
+    const { useConfirmed } = this.state
+    this.setState({ useConfirmed: !useConfirmed })
+    API.setUseConfirmed(!useConfirmed)
   }
 
   render () {
     const { status } = this.props
+    const { useConfirmed } = this.state
     return (
       <Grid
         container
@@ -23,6 +42,21 @@ class Settings extends React.Component {
         alignItems='center'
       >
         <StatusAlert status={status} />
+        <List component='nav' style={{ marginTop: 25, width: '100%' }} aria-label='settings'>
+          <ListItem button onClick={this.toggleUseConfirmed.bind(this)}>
+            <ListItemIcon>
+              {
+                useConfirmed
+                  ? (
+                    <CheckBoxIcon />
+                  ) : (
+                    <CheckBoxOutlineBlankOutlinedIcon />
+                  )
+              }
+            </ListItemIcon>
+            <ListItemText primary='Use only confirmed diagnoses' />
+          </ListItem>
+        </List>
       </Grid>
     )
   }
