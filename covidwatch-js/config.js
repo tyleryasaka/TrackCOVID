bf156f5c780c2f7095fbd1f31c5e3a08309c1554
Tyler Yasaka <contact@tyleryasaka.me>
2020-04-02 01:52:03 -0700

    Use relative urls

--- covidwatch-js/config.js
+++ covidwatch-js/config.js
@@ -1,7 +1,7 @@
 const oneDay = 1000 * 60 * 60 * 24
 const safetyPeriod = 14 * oneDay
 const estimatedDiagnosisDelay = 2 * oneDay
-const serverBaseUrl = 'https://trackcovid.net/api/checkpoints'
+const serverBaseUrl = '/api/checkpoints'
 
 module.exports = {
   safetyPeriod,
