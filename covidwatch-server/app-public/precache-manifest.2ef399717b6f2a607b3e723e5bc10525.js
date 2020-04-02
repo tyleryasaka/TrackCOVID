3bdaa4b611100056139d142b3d8a07a160319738
Tyler Yasaka <contact@tyleryasaka.me>
2020-04-02 00:37:10 -0700

    Build

---
+++ covidwatch-server/app-public/precache-manifest.2ef399717b6f2a607b3e723e5bc10525.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3218cd7731a71939f458e1badf42c97",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "cd46ed7681b7e9e15feb",
+    "url": "/app/static/js/2.2e4a3d8a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/app/static/js/2.2e4a3d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f5ecf554486410caf2b",
+    "url": "/app/static/js/main.c2b3f964.chunk.js"
+  },
+  {
+    "revision": "2f013ec05311f8084715",
+    "url": "/app/static/js/runtime-main.d2cf8259.js"
+  }
+]);
---
+++ covidwatch-server/app-public/precache-manifest.c9514ab1f0700f87a09814056564d167.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8155f37421a993b091782ac9b6f023a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd46ed7681b7e9e15feb",
+    "url": "/static/js/2.2e4a3d8a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2e4a3d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f5ecf554486410caf2b",
+    "url": "/static/js/main.c2b3f964.chunk.js"
+  },
+  {
+    "revision": "9114a68825ab645d0f51",
+    "url": "/static/js/runtime-main.8eb9c53b.js"
+  }
+]);
